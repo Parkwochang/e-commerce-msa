b3a0b424c38132b06408abc4d38173017634d7e4
Parkchangwoo <whwh3560@gmail.com>
2026-01-08 07:55:49 +0900

    fix: 변경사항 전체 반영

--- packages/config/src/grpc/grpc.module.ts
+++ packages/config/src/grpc/grpc.module.ts
@@ -5,25 +5,10 @@ import { createGrpcClient } from './grpc-client.factory';
 
 @Module({})
 export class GrpcModule {
-  static forRoot(options: { url: string; protoPath: string; packageName: string }) {
+  static forRoot(options: { url: string; protoPath: string; packageName: string; name: string }) {
     return {
       module: GrpcModule,
       imports: [ClientsModule.register([createGrpcClient(options)])],
     };
   }
 }
-
-@Module({
-  imports: [
-    ClientsModule.register([
-      createGrpcClient({
-        name: 'USER_SERVICE',
-        url: process.env.USER_SVC_GRPC_URL || 'localhost:50051',
-        protoPath: join(__dirname, '../protos/user.proto'),
-        packageName: 'user',
-      }),
-    ]),
-  ],
-  exports: [ClientsModule],
-})
-export class UserGrpcModule {}
